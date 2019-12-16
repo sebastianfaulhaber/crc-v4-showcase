66677fb5fc81710edbfc3741f4b614b66d4e6dc2
sfaulhab <sfaulhab@redhat.com>
2019-12-16 16:07:37 +0100

    Added creation of a demo project and Pull Secret.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crc-v4-showcase
+
+## Create Pull secret for Red Hat Container Catalog
+1. Create a new Registry Service Accounts here: https://access.redhat.com/terms-based-registry/#/accounts
+2. Download the OpenShift Secret and save it to `config/crc-secret.yaml`
--- customise_crc.sh
+++ customise_crc.sh
@@ -1,18 +1,24 @@
 # Extract credentials for kubeadmin
 KUBEADMIN_PWD=`crc console --credentials |sed '$!d' | sed -e "s/'//g" | sed -e "s/\.//g" | awk 'NF>1{print $NF}'`
+CRC_ADMIN_URL='https://api.crc.testing:6443'
 
 # Login with kubeadmin
-oc login -u kubeadmin -p $KUBEADMIN_PWD https://api.crc.testing:6443
+oc login -u kubeadmin -p $KUBEADMIN_PWD $CRC_ADMIN_URL
 
 # Create admin user for htpasswd Provider (see https://docs.openshift.com/container-platform/4.2/authentication/identity_providers/configuring-htpasswd-identity-provider.html)
 htpasswd -c -B -b users.htpasswd developer developer && htpasswd -b users.htpasswd admin admin
 oc delete secret htpass-secret -n openshift-config
 oc create secret generic htpass-secret --from-file=htpasswd=./users.htpasswd -n openshift-config
-sleep 5
+sleep 10
 oc adm policy add-cluster-role-to-user cluster-admin admin
 rm ./users.htpasswd
 
-#Login with admin and remove kubeadmin user
+# Login with admin and remove kubeadmin user
 sleep 2
-oc login -u admin -p admin https://api.crc.testing:6443
+oc login -u admin -p admin $CRC_ADMIN_URL
 oc delete secrets kubeadmin -n kube-system
+
+# Create demo project
+oc new-project company-website --display-name='Company Website' --description='My new company website based on a modern cloud native development stack.' --as=developer --as-group=system:authenticated --as-group=system:authenticated:oauth
+# Create Pull Secret for Red Hat Container Catalog
+kubectl create -f config/crc-secret.yaml --namespace=company-website
